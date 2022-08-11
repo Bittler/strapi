e752b6efb73beca9a4f0aea32e424f12069de3c2
idkzh <109007210+idkzh@users.noreply.github.com>
2022-08-11 01:19:05 -0300

    Update strapi.py

--- strapi.py
+++ strapi.py
@@ -17,9 +17,10 @@ class Strapi:
 
     def auth(self, username: str, password: str) -> None:
         if (self.token == None):
-            self.token = self.call("POST", "/auth/local", data={"identifier": username, "password": password}).get("jwt")
+            auth = self.call("POST", "/auth/local", data={"identifier": username, "password": password})
+            self.token = auth.get("jwt")
             if (self.token == None):
-                raise Exception("Username/Password invalid.")
+                raise Exception(auth)
 
     def query(self, query: dict) -> dict:
         return self.call("POST", "/graphql", data={"query": query}).get("data")
